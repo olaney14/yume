1f49b354a7eecab8131e4d622a5c5b35feaec0b6
meatinloafform <meatinloafform@gmail.com>
2025-01-08 15:26:34 -0600

    Prism world
    Package lib files in project
    Rearrange nexus and worlds
    New music

---
+++ res/maps/tilesets/prisms.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="prisms" tilewidth="16" tileheight="16" tilecount="192" columns="12">
+ <image source="../../textures/tiles/prisms.png" width="192" height="256"/>
+ <tile id="144">
+  <properties>
+   <property name="blocking" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
