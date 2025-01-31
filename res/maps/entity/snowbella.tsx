f65e7380a49912027330c59337193973c01ec6dc
meatinloafform <meatinloafform@gmail.com>
2025-01-31 13:49:09 -0600

    Snow city +

---
+++ res/maps/entity/snowbella.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="snowbella" tilewidth="16" tileheight="32" tilecount="5" columns="5">
+ <image source="../../textures/entity/snowbella.png" width="80" height="32"/>
+</tileset>
--- res/maps/tilesets/snow_city.tsx
+++ res/maps/tilesets/snow_city.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="snow_city" tilewidth="16" tileheight="16" tilecount="288" columns="16">
- <image source="../../textures/tiles/snow_city.png" width="256" height="288"/>
+<tileset version="1.10" tiledversion="1.11.0" name="snow_city" tilewidth="16" tileheight="16" tilecount="352" columns="16">
+ <image source="../../textures/tiles/snow_city.png" width="256" height="352"/>
  <tile id="0">
   <properties>
    <property name="blocking" type="bool" value="true"/>
