c7ff10958ca31f6fd1be376bcf0ebf6013b54d7d
meatinloafform <meatinloafform@gmail.com>
2025-03-22 12:06:09 -0500

    start of back room

---
+++ res/maps/tilesets/worn_tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="worn_tiles" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../textures/tiles/worn_tiles.png" width="128" height="128"/>
+ <tile id="4">
+  <properties>
+   <property name="blocking" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
