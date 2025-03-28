0856e8eeb58366db2c5292636dc1caada475e1ea
meatinloafform <meatinloafform@gmail.com>
2025-03-28 00:45:22 -0500

    laptop

---
+++ res/maps/entity/carrot.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="carrot" tilewidth="64" tileheight="64" tilecount="4" columns="4">
+ <image source="../../textures/entity/carrot.png" width="256" height="64"/>
+</tileset>
