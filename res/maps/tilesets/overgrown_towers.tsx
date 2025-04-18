eae826e38f4077c8d5af76b93b2034d4de9c2a43
meatinloafform <meatinloafform@gmail.com>
2025-04-18 12:00:06 -0500

    Change save music

--- res/maps/tilesets/overgrown_towers.tsx
+++ res/maps/tilesets/overgrown_towers.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="overgrown_towers" tilewidth="16" tileheight="16" tilecount="72" columns="4">
  <image source="../../textures/tiles/overgrown_towers.png" width="64" height="288"/>
+ <tile id="13">
+  <properties>
+   <property name="step" value="step_metal"/>
+  </properties>
+ </tile>
  <tile id="22">
   <properties>
    <property name="blocking" type="bool" value="true"/>
