caeeed0459d25556f6d3f0c35a8fb365221a9b61
meatinloafform <meatinloafform@gmail.com>
2026-05-09 16:15:49 -0500

    begin of summer commit

--- res/maps/tilesets/scrapyard.tsx
+++ res/maps/tilesets/scrapyard.tsx
@@ -1,159 +1,169 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="scrapyard" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../../textures/tiles/scrapyard.png" width="256" height="256"/>
+ <tile id="1">
+  <properties>
+   <property name="blocking" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="25">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="26">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="29">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="30">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="45">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="46">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="57">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="58">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="73">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="74">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="75">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="76">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="89">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="90">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="91">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="92">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="93">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="94">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="95">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="105">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="106">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="107">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="108">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="109">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="110">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="111">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="123">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="124">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="125">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="126">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
  <tile id="127">
   <properties>
-   <property name="step" value="step1"/>
+   <property name="step" value="step8_water"/>
   </properties>
  </tile>
 </tileset>
